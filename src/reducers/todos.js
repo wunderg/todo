f925e17878a2e34dd313b0ef0cbafc66ccf47465
darwin <oleg.umarov@gmail.com>
2016-02-27 19:19:56 -0600

    added files

---
+++ src/reducers/todos.js
@@ -0,0 +1,18 @@
+import { INCREMENT, DECREMENT, RESET } from '../actions';
+
+const INITIAL_STATE = {
+  count: 0
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch(action.type) {
+    case INCREMENT:
+      return {...state, count: action.payload + 1}
+    case DECREMENT:
+      return {...state, count: action.payload - 1}
+    case RESET:
+      return {...state, count: 0}
+    default:
+      return state
+  }
+}
