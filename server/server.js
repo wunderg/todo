e1e9c8fb6d062f40bd681281aa1e893edb07c8d0
darwin <oleg.umarov@gmail.com>
2016-02-28 15:49:55 -0600

    clean up

--- server/server.js
+++ server/server.js
@@ -1,76 +1,53 @@
-import webpack from 'webpack';
-import webpackMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import webpackConfig from '../webpack-config.js';
-
 import express from 'express';
 import path from 'path';
-import http from 'http';
+import webpack from 'webpack';
+import WebpackDevServer from 'webpack-dev-server';
 import bodyParser from 'body-parser';
-
-import Router from './router';
-
+import webpackConfig from '../webpack-config.js';
 
 const isProduction = process.env.NODE_ENV === 'production';
 const isDeveloping = !isProduction;
+const port = isProduction ? (process.env.PORT || 80) : 3000;
 
 const app = express();
 
-// Router(app, express);
+const staticPath = path.join(__dirname, '../');
 
-/* Webpack Config */
-if (isDeveloping) {
-  const WEBPACK_PORT = 3001;
-  const compiler = webpack(webpackConfig);
+app.use(bodyParser());
 
-  app.use(webpackMiddleware(compiler, {
-    publicPath: webpackConfig.output.publicPath,
-    stats: {
-      color: true,
-      hash: false,
-      timings: true,
-      chunks: false,
-      chunkModules: false,
-      modules: false
-    }
-  }));
+app.use(express.static(staticPath));
 
-  app.use(webpackHotMiddleware(compiler));
-  app.listen(WEBPACK_PORT, 'localhost', (err) => {
-    if (err) {
-      console.log(err);
-    }
-
-    console.log(`WebpackDevServer listening at port ${WEBPACK_PORT}`);
+app.get('*', (req, res) => {
+  res.sendFile('index.html', {
+    root: staticPath
   });
-}
-
-
-/* API config */
-const publicPath = path.resolve(__dirname, '../');
-app.use(bodyParser.json({ type: 'application/json' }));
-app.use(express.static(publicPath));
-
-const port = isProduction ? (process.env.PORT || 80) : 3000;
-
-
-
-
-{/*  API Routes */}
-app.get('*', (request, response) => {
-  console.log(request);
-  response.sendFile(path.resolve(__dirname, '../', 'index.html'));
 });
 
-
-
-/*  Basic HTTP Proxy to pass thru websockets requests*/
-const server = http.createServer(app);
-
-server.listen(port, (err) => {
+app.listen(port, err => {
   if (err) {
     console.log(err);
   }
-
-  console.log(`Server running on port ${port}`);
+  console.log('Server at 3001');
 });
+
+if (isDeveloping) {
+  const devServer = new WebpackDevServer(webpack(webpackConfig), {
+    publicPath: webpackConfig.output.publicPath,
+    inline: true,
+    hot: true,
+    quiet: false,
+    noInfo: false,
+    stats: { colors: true },
+    proxy: [{
+      path: /\api(.*)/,
+      target: 'http://localhost: 3000'
+    }]
+  });
+
+  devServer.listen(3001, 'localhost', err => {
+    if (err) {
+      console.log(err);
+    }
+    console.log('DevServer at 3001');
+  });
+}
